7e77c5647bd014e386ba513d7d268aa4d38a5339
roidaradal <johnroy.daradal@goodkredit.com>
2025-01-14 15:43:28 +0800

    v0.6.1 - Run SelectRowQuery

--- errors.go
+++ errors.go
@@ -6,3 +6,9 @@ var (
 	errNotStruct        = errors.New("not a struct")
 	errIncompleteFields = errors.New("incomplete fields")
 )
+
+var (
+	errEmptyQuery     = errors.New("empty query")
+	errNoDBConnection = errors.New("nil db connection")
+	errNoRowReader    = errors.New("nil row reader")
+)
--- query_select1.go
+++ query_select1.go
@@ -1,9 +1,13 @@
 package rdb
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+)
 
 type selectRowQuery[T any] struct {
 	selectQuery[T]
+	reader rowReader[T]
 }
 
 /*
@@ -20,6 +24,35 @@ func (q *selectRowQuery[T]) Build() (string, []any) {
 }
 
 /*
+Input: reader function
+*/
+func (q *selectRowQuery[T]) SetReader(reader rowReader[T]) {
+	q.reader = reader
+}
+
+/*
+Input: initialized DB connection
+
+Constraint: Need to call SetReader() first, otherwise nothing happens
+
+Output: &struct that contains reader data, error
+*/
+func (q *selectRowQuery[T]) Run(dbc *sql.DB) (*T, error) {
+	query, values := q.Build()
+	if query == "" {
+		return nil, errEmptyQuery
+	}
+	if dbc == nil {
+		return nil, errNoDBConnection
+	}
+	if q.reader == nil {
+		return nil, errNoRowReader
+	}
+	row := dbc.QueryRow(query, values...)
+	return q.reader(row)
+}
+
+/*
 Input: &struct, table (string)
 
 Note: Same &struct will be used for setting conditions later
@@ -30,5 +63,6 @@ func NewSelectRowQuery[T any](object *T, table string) *selectRowQuery[T] {
 	q := selectRowQuery[T]{}
 	q.initialize(object, table)
 	q.columns = make([]string, 0)
+	q.reader = nil
 	return &q
 }
