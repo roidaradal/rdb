cc8be8ee2aae8086fbc256c9cbcf8e7a97be18e5
roidaradal <johnroy.daradal@goodkredit.com>
2025-01-14 18:17:40 +0800

    v0.7.1 - Add Time Functions

--- time.go
+++ time.go
@@ -20,3 +20,23 @@ func DateTimeNow() DateTime {
 func TimestampNow() string {
 	return TimeNow().Format(timestampFormat)
 }
+
+func DateTimeNowWithExpiry(duration time.Duration) (DateTime, DateTime) {
+	now := TimeNow()
+	expiry := now.Add(duration)
+	return DateTime(now.Format(standardFormat)), DateTime(expiry.Format(standardFormat))
+}
+
+func ParseTime(datetime DateTime) (time.Time, error) {
+	timezone, _ := time.LoadLocation(currentTimezone)
+	return time.ParseInLocation(standardFormat, string(datetime), timezone)
+}
+
+func CheckIfExpired(expiry DateTime) bool {
+	limit, err := ParseTime(expiry)
+	if err != nil {
+		return true // default to expired if invalid datetime
+	}
+	now := TimeNow()
+	return now.After(limit)
+}
