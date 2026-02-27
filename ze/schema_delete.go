3d9f93f191e0d9327a7bdc28ee134778cb9ed480
roidaradal <johnroy.daradal@goodkredit.com>
2026-02-27 14:28:44 +0800

    v0.14.14 - Schema CountDelete

--- ze/schema_delete.go
+++ ze/schema_delete.go
@@ -9,31 +9,55 @@ import (
 
 // DeleteQuery at schema.Table
 func (s Schema[T]) Delete(rq *Request, condition rdb.Condition) error {
-	return deleteAt(rq, condition, s.Name, s.Table, false)
+	_, err := deleteAt(rq, condition, s.Name, s.Table, false)
+	return err
 }
 
 // DeleteQuery at table
 func (s Schema[T]) DeleteAt(rq *Request, condition rdb.Condition, table string) error {
-	return deleteAt(rq, condition, s.Name, table, false)
+	_, err := deleteAt(rq, condition, s.Name, table, false)
+	return err
 }
 
 // DeleteQuery transaction at schema.Table
 func (s Schema[T]) DeleteTx(rqtx *Request, condition rdb.Condition) error {
-	return deleteAt(rqtx, condition, s.Name, s.Table, true)
+	_, err := deleteAt(rqtx, condition, s.Name, s.Table, true)
+	return err
 }
 
 // DeleteQuery transaction at table
 func (s Schema[T]) DeleteTxAt(rqtx *Request, condition rdb.Condition, table string) error {
+	_, err := deleteAt(rqtx, condition, s.Name, table, true)
+	return err
+}
+
+// DeleteQuery at schema.Table, return rowsAffected
+func (s Schema[T]) CountDelete(rq *Request, condition rdb.Condition) (int, error) {
+	return deleteAt(rq, condition, s.Name, s.Table, false)
+}
+
+// DeleteQuery at table, return rowsAffected
+func (s Schema[T]) CountDeleteAt(rq *Request, condition rdb.Condition, table string) (int, error) {
+	return deleteAt(rq, condition, s.Name, table, false)
+}
+
+// DeleteQuery transaction at schema.Table, return rowsAffected
+func (s Schema[T]) CountDeleteTx(rqtx *Request, condition rdb.Condition) (int, error) {
+	return deleteAt(rqtx, condition, s.Name, s.Table, true)
+}
+
+// DeleteQuery transaction at table, return rowsAffected
+func (s Schema[T]) CountDeleteTxAt(rqtx *Request, condition rdb.Condition, table string) (int, error) {
 	return deleteAt(rqtx, condition, s.Name, table, true)
 }
 
 // Common: create and execute DeleteQuery at given table using condition
-func deleteAt(rq *Request, condition rdb.Condition, name, table string, isTx bool) error {
+func deleteAt(rq *Request, condition rdb.Condition, name, table string, isTx bool) (int, error) {
 	// Check that condition is set
 	if condition == nil {
 		rq.AddLog("Delete condition is not set")
 		rq.Status = Err500
-		return fail.MissingParams
+		return 0, fail.MissingParams
 	}
 
 	// Build DeleteQuery
@@ -52,12 +76,12 @@ func deleteAt(rq *Request, condition rdb.Condition, name, table string, isTx boo
 	if err != nil {
 		rq.AddFmtLog("Failed to delete %s", name)
 		rq.Status = Err500
-		return err
+		return 0, err
 	}
 
 	rowsAffected := rdb.RowsAffected(result)
 	if rowsAffected != 1 {
 		rq.AddFmtLog("Deleted: %d %s", rowsAffected, name)
 	}
-	return nil
+	return rowsAffected, nil
 }
